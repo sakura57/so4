cc64c8dd84099ca9a85fde5dd0c7d0f8905acda2
Jacob Farnsworth <6502enthusiast@gmail.com>
2020-01-28 19:14:27 -0500

    Added console commands for money, metal, and giving items

--- spacegame7/DebugCommands.cxx
+++ spacegame7/DebugCommands.cxx
@@ -51,7 +51,7 @@ void populate_debug_commands()
 				spawnPosition.x = std::stof(args[2]);
 				spawnPosition.y = std::stof(args[3]);
 			}
-			catch(std::exception & e)
+			catch(std::invalid_argument & e)
 			{
 				inputValid = false;
 			}
@@ -88,7 +88,7 @@ void populate_debug_commands()
 			{
 				baseId = std::stoi(args[1]);
 			}
-			catch(std::exception & e)
+			catch(std::invalid_argument & e)
 			{
 				inputValid = false;
 			}
@@ -126,7 +126,7 @@ void populate_debug_commands()
 				spawnPosition.x = std::stof(args[1]);
 				spawnPosition.y = std::stof(args[2]);
 			}
-			catch(std::exception & e)
+			catch(std::invalid_argument & e)
 			{
 				inputValid = false;
 			}
@@ -155,6 +155,138 @@ void populate_debug_commands()
 
 				pWorld->end_world_transaction();
 			}
+		}
+	);
+
+	pDebugConsole->console_register_command(
+		"cash",
+		[pDebugConsole](std::vector<std::string> const& args) {
+			unsigned int const expectedArguments = 2;
+
+			if(args.size() != expectedArguments)
+			{
+				pDebugConsole->console_write_line("Expected " + Conversion::uint_to_string(expectedArguments - 1) + " arguments");
+
+				return;
+			}
+
+			bool inputValid = true;
+			unsigned int cash;
+
+			try
+			{
+				cash = std::stoi(args[1]);
+			}
+			catch(std::invalid_argument & e)
+			{
+				inputValid = false;
+			}
+
+			if(!inputValid)
+			{
+				pDebugConsole->console_write_line("Invalid argument format");
+			}
+			else
+			{
+				pDebugConsole->console_write_line("OK");
+
+				SG::get_intransient_data_manager()->get_character_entity_manager()->get_player_character_entity()->get_inventory()->adjust_money(cash);
+			}
+		}
+	);
+
+	pDebugConsole->console_register_command(
+		"metal",
+		[pDebugConsole](std::vector<std::string> const& args) {
+			unsigned int const expectedArguments = 2;
+
+			if(args.size() != expectedArguments)
+			{
+				pDebugConsole->console_write_line("Expected " + Conversion::uint_to_string(expectedArguments - 1) + " arguments");
+
+				return;
+			}
+
+			bool inputValid = true;
+			unsigned int metal;
+
+			try
+			{
+				metal = std::stoi(args[1]);
+			}
+			catch(std::invalid_argument & e)
+			{
+				inputValid = false;
+			}
+
+			if(!inputValid)
+			{
+				pDebugConsole->console_write_line("Invalid argument format");
+			}
+			else
+			{
+				pDebugConsole->console_write_line("OK");
+
+				SG::get_intransient_data_manager()->get_character_entity_manager()->get_player_character_entity()->get_inventory()->adjust_metal(metal);
+			}
+		}
+	);
+
+	pDebugConsole->console_register_command(
+		"give",
+		[pDebugConsole](std::vector<std::string> const& args) {
+			unsigned int const expectedArguments = 3;
+
+			if(args.size() != expectedArguments)
+			{
+				pDebugConsole->console_write_line("Expected " + Conversion::uint_to_string(expectedArguments - 1) + " arguments");
+
+				return;
+			}
+
+			bool inputValid = true;
+			ArchId arch;
+			unsigned int quantity;
+
+			try
+			{
+				arch = std::stoi(args[1]);
+				quantity = std::stoi(args[2]);
+			}
+			catch(std::invalid_argument & e)
+			{
+				inputValid = false;
+			}
+
+			if(!inputValid)
+			{
+				pDebugConsole->console_write_line("Invalid argument format");
+			}
+			else
+			{
+				pDebugConsole->console_write_line("OK");
+
+				SG::get_intransient_data_manager()->get_character_entity_manager()->get_player_character_entity()->get_inventory()->add_item(arch, quantity);
+			}
+		}
+	);
+
+	pDebugConsole->console_register_command(
+		"script",
+		[pDebugConsole](std::vector<std::string> const& args) {
+			unsigned int const expectedArguments = 2;
+
+			if(args.size() != expectedArguments)
+			{
+				pDebugConsole->console_write_line("Expected " + Conversion::uint_to_string(expectedArguments - 1) + " arguments");
+
+				return;
+			}
+
+
+			pDebugConsole->console_write_line("OK");
+
+			SG::get_script_engine()->script_enqueue(args[1].c_str());
 		}
 	);
 
