b6f05f3f6d2065210ff58592cc377de8210034b9
Jacob Farnsworth <6502enthusiast@gmail.com>
2020-02-06 21:40:54 -0500

    Fix player death panel position

--- spacegame7/PlayerDiedPanel.cxx
+++ spacegame7/PlayerDiedPanel.cxx
@@ -6,7 +6,7 @@ int PlayerDiedPanel::m_iPanelInstances = 0;
 
 void PlayerDiedPanel::render_panel(float const flDelta)
 {
-	ImGui::SetNextWindowPosCenter(ImGuiCond_Once | ImGuiCond_Appearing);
+	ImGui::SetNextWindowPosCenter();
 
 	ImGui::Begin(
 		"You are dead",
