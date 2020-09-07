b4c0dcb3d010d303009d2ffe3bc4d69b0c0a2fb4
Jacob Farnsworth <6502enthusiast@gmail.com>
2020-09-07 08:32:27 -0400

    Add an encounter in Callisto Gamma

--- assets/scripts/systems/callisto_gamma.lua
+++ assets/scripts/systems/callisto_gamma.lua
@@ -28,6 +28,77 @@ local patroller2_x, patroller2_y = sgs_random_float(1000.0, 3000.0), sgs_random_
 
 local patroller2 = sgs_ship_create(patroller2_char, patroller2_x, patroller2_y, 200.0)
 
+local player_ship = sgs_world_get_player()
+
+local borderpatrol1 = 0
+local borderpatrol2 = 0
+local borderpatrol3 = 0
+
+sgs_enqueue_callback(2.0, "test_spawn_border_patrol")
+
+function test_spawn_border_patrol()
+	if not sgs_object_exists(player_ship) then
+		return
+	end
+	
+	if sgs_get_variable("cg_encounter_border_patrol") == "y" then
+		return
+	end
+	
+	local playerx, playery = sgs_object_get_position(player_ship)
+	
+	borderpatrol1 = sgs_ship_create(33, playerx + 800.0, playery, 180.0)
+	borderpatrol2 = sgs_ship_create(34, playerx + 800.0, playery + 350.0, 220.0)
+	borderpatrol3 = sgs_ship_create(35, playerx + 800.0, playery - 350.0, 140.0)
+	
+	sgs_effect_jump_in(playerx + 800.0, playery)
+	sgs_effect_jump_in(playerx + 800.0, playery + 350.0)
+	sgs_effect_jump_in(playerx + 800.0, playery - 350.0)
+	
+	sgs_hud_hide()
+	sgs_object_halt(player_ship)
+	sgs_cam_begin_vignette_camera(2.0, playerx, playery, playerx + 800.0, playery)
+	sgs_enqueue_callback(2.0, "encounter_border_patrol_start_dialogue")
+end
+
+function encounter_border_patrol_start_dialogue()
+	if not sgs_object_exists(player_ship) then
+		return
+	end
+	
+	if not sgs_object_exists(borderpatrol1) then
+		return
+	end
+	
+	sgs_dialogue_begin(49)
+	sgs_enqueue_callback(1.0, "encounter_border_patrol_dialogue_ended")
+end
+
+function encounter_border_patrol_dialogue_ended()
+	if not sgs_object_exists(player_ship) then
+		return
+	end
+	
+	if not sgs_object_exists(borderpatrol1) then
+		return
+	end
+	
+	sgs_cam_begin_chase_camera(player_ship)
+	sgs_hud_show()
+	
+	sgs_set_variable("cg_encounter_border_patrol", "y")
+	
+	if sgs_get_variable("cg_border_patrol_provoked") == "y" then
+		sgs_object_set_attitude(borderpatrol1, player_ship, -0.7)
+		sgs_object_set_attitude(borderpatrol2, player_ship, -0.7)
+		sgs_object_set_attitude(borderpatrol3, player_ship, -0.7)
+	else
+		sgs_object_goto_directive(borderpatrol1, -2500.0, 500.0, 500.0)
+		sgs_object_goto_directive(borderpatrol2, -2500.0, 850.0, 500.0)
+		sgs_object_goto_directive(borderpatrol3, -2500.0, 150.0, 500.0)
+	end
+end
+
 function patroller1_wander()
 	if not sgs_object_exists(patroller1) then
 		return
